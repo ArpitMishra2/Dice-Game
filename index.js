a2444f9e8ae7718ac09b4df1eec12d9b7428c608
ArpitMishra2 <124170209+ArpitMishra2@users.noreply.github.com>
2023-05-11 12:20:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var randomNumber1 = Math.floor(Math.random() * 6 + 1); // By this we will get a random number from 1-6
 var randomDiceImage = "dice"+ randomNumber1 +".png";   // This select the type of image used
-var randomImageSource = "./images/" + randomDiceImage;   // This will show a random image on the page.
+var randomImageSource = "images/" + randomDiceImage;   // This will show a random image on the page.
 var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src", randomImageSource);
 
@@ -8,7 +8,7 @@ image1.setAttribute("src", randomImageSource);
 
 var randomNumber2 = Math.floor(Math.random() * 6 + 1);
 var randomDiceImage2 = "dice"+ randomNumber2 +".png";
-var randomImageSource2 = "./images/" + randomDiceImage2;
+var randomImageSource2 = "images/" + randomDiceImage2;
 var image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src", randomImageSource2);
 
